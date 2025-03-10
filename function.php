6aa7120859d5afe82df6884f99079ca4e25d23b5
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-03-10 17:13:23 +0500

    fix: file structure for style.css rendering

--- function.php
+++ function.php
@@ -1,6 +1,7 @@
 <?php
-function my_theme_enqueue_styles() {
-    wp_enqueue_style('main-style', get_stylesheet_uri());
+function my_theme_enqueue_assets() {
+    wp_enqueue_style('main-style', get_stylesheet_uri()); 
+    wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/style.css'); 
 }
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_styles');
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_assets');
 ?>
