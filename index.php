8e5c237da3fc4bec2fabf259db8f42b897ab3fd8
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-03-10 17:02:14 +0500

    fix: add necessary templates within the files

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kumsuat</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php get_header(); ?>
+
+<main>
+    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+        <article>
+            <h2><?php the_title(); ?></h2>
+            <div><?php the_content(); ?></div>
+        </article>
+    <?php endwhile; endif; ?>
+</main>
+
+<?php get_footer(); ?>
