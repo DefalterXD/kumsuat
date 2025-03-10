922cd8be1c825eb7bf5fa20058e1d480fb547bae
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-03-10 17:49:31 +0500

    feat: add front page

---
+++ front-page.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+    <h1>Welcome to My Custom Home Page</h1>
+    <p>This is my own homepage, separate from the blog.</p>
+</main>
+
+<?php get_footer(); ?>
--- function.php
+++ function.php
@@ -1,5 +1,6 @@
 <?php
 function my_theme_enqueue_assets() {
+    register_nav_menu('main-menu' __('Main Menu'));
     wp_enqueue_style('main-style', get_stylesheet_uri()); 
     wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/style.css'); 
 }
