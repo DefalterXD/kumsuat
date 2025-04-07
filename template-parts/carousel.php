595d66609021079f5bffb582b966a95b593c5f5c
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-04-08 04:43:24 +0500

    feat: add carousel template

---
+++ template-parts/carousel.php
@@ -0,0 +1,64 @@
+<section class="carousel__container">
+        <div class="carousel__card one ">
+            <div class="carousel__left-side">
+                <div class="carousel__text__container">
+                    <h1 class="carousel__header">Качественное оборудование для тестирования</h1>
+                    <p class="carousel__paragraph">Мы гарантируем самый точный результат теста</p>
+                </div>
+                <div class="carousel__navigation">
+                    <button class="arrow prev">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+                    </button>
+                    <button class="arrow next">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+                    </button>
+                </div>
+            </div>
+            <div class="carousel__right-side">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/URS-11-strips.jpg" alt="product__image">
+            </div>
+        </div>
+        <div class="carousel__card two hidden__card ">
+            <div class="carousel__left-side">
+                <div class="carousel__text__container">
+                <h1 class="carousel__header">Чай оборудование для тестирования</h1>
+                <p class="carousel__paragraph">Мы гарантируем самый точный результат теста</p>
+            </div>
+            <div class="carousel__navigation">
+                <button class="arrow prev">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+                </button>
+                <button class="arrow next">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+                </button>
+            </div>
+        </div>
+        <div class="carousel__right-side">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/URS-11-strips.jpg" alt="product__image">
+        </div>
+    </div>
+    <div class="carousel__card three hidden__card ">
+        <div class="carousel__left-side">
+            <div class="carousel__text__container">
+                <h1 class="carousel__header">Работа для тестирования</h1>
+                <p class="carousel__paragraph">Мы гарантируем самый точный результат теста. Мы гарантируем самый точный результат теста</p>
+            </div>
+            <div class="carousel__navigation">
+                <button class="arrow prev">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
+                </button>
+                <button class="arrow next">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+                </button>
+            </div>
+        </div>
+        <div class="carousel__right-side">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/URS-11-strips.jpg" alt="product__image">
+        </div>
+    </div>
+    <div class="carousel__indicator__container">
+        <span class="round__indicator one round__indicator__active"></span>
+        <span class="round__indicator two" ></span>
+        <span class="round__indicator three"></span>
+    </div>
+</section>
