70f3932ae75c08973bea5afb5fc4e525d1c7acbb
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-04-08 04:44:24 +0500

    feat: add r&d template for index

---
+++ template-parts/index__r&d.php
@@ -0,0 +1,6 @@
+<section class="rd__container">
+    <div class="rd__card__container">
+        <h1 class="rd__card__title">R&D</h1>
+        <h3 class="rd__card__desc">Explore our lab, where we conduct rigorous testing and quality control to ensure the highest standards in our in vitro diagnostic test papers.</h3>
+    </div>
+</section>
