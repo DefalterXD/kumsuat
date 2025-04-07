5e092b46e7dc1e13671fed395749ef46dd60d81a
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-04-08 04:37:05 +0500

    feat: enque assets automatically

--- function.php
+++ function.php
@@ -1,8 +1,23 @@
 <?php
-function my_theme_enqueue_assets() {
-    register_nav_menu('main-menu' __('Main Menu'));
-    wp_enqueue_style('main-style', get_stylesheet_uri()); 
-    wp_enqueue_style('custom-style', get_template_directory_uri() . '/assets/css/style.css'); 
+function auto_enqueue_assets() {
+    $css_path = get_template_directory() . '/assets/css/';
+    $js_path = get_template_directory() . '/assets/js/';
+
+    $css_files = scandir($css_path);
+    $js_files = scandir($js_path);
+
+    foreach ($css_files as $file) {
+        if (pathinfo($file, PATHINFO_EXTENSION) === 'css') {
+            wp_enqueue_style('custom-' . basename($file, '.css'), get_template_directory_uri() . '/assets/css/' . $file, array(), null, 'all');
+        }
+    }
+
+    foreach ($js_files as $file) {
+        if (pathinfo($file, PATHINFO_EXTENSION) === 'js') {
+            wp_enqueue_script('custom-' . basename($file, '.js'), get_template_directory_uri() . '/assets/js/' . $file, array(), null, true);
+        }
+    }
 }
-add_action('wp_enqueue_scripts', 'my_theme_enqueue_assets');
+
+add_action('wp_enqueue_scripts', 'auto_enqueue_assets');
 ?>
