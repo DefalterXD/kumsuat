ddf9b9ce30f760512a92c59a1e52019d878cd926
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-04-08 04:45:19 +0500

    fix: add template parts into the page

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php get_template_part('./template-parts/header'); ?>
 
+<?php get_template_part('./template-parts/carousel'); ?>
+<?php get_template_part('./template-parts/index__products'); ?>
+<?php get_template_part('./template-parts/index__r&d'); ?>
+
 <?php get_template_part('./template-parts/footer'); ?>
