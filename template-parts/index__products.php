213a2b5773a5e8a62f2c1f9595779c6df8531630
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-04-08 04:43:51 +0500

    feat: add product template for index

---
+++ template-parts/index__products.php
@@ -0,0 +1,29 @@
+<section class="products__catalog__container">
+    <h1 class="products__catalog__title">Products</h1>
+    <h3 class="products__catalog__desc__title">Innovative Diagnostic Solution</h3>
+    <div class="products__catalog__select__container">
+        <span class="product__catalog__select">Professional Use</span>
+        <span class="product__catalog__select">Self-Testing Use</span>
+        <span class="product__catalog__select">Other Test Strips</span>
+    </div>
+    <div class="products__catalog__cards__container">
+         <div class="product__catalog__card__container">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/URS-11-strips.jpg" alt="product__image">
+            <h1 class="product__catalog__card__title">Name of the product</h1>
+            <h3 class="product__catalog__card__desc">The specific description of this product</h3>
+            <button class="product__catalog__card__btn"><a href="" class="product__card__link">View more &rarr;</a></button>
+         </div>
+         <div class="product__catalog__card__container">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/URS-11-strips.jpg" alt="product__image">
+            <h1 class="product__catalog__card__title">Name of the product</h1>
+            <h3 class="product__catalog__card__desc">The specific description of this product</h3>
+            <button class="product__catalog__card__btn"><a href="" class="product__card__link">View more &rarr;</a></button>
+         </div>
+         <div class="product__catalog__card__container">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/URS-11-strips.jpg" alt="product__image">
+            <h1 class="product__catalog__card__title">Name of the product</h1>
+            <h3 class="product__catalog__card__desc">The specific description of this product</h3>
+            <button class="product__catalog__card__btn"><a href="" class="product__card__link">View more &rarr;</a></button>
+         </div>
+    </div>
+</section>
