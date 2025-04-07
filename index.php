c4351db2c41bb5c0cced93a0c07435b08acc99c4
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-04-08 04:38:28 +0500

    feat: use initial template parts

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<?php get_header(); ?>
+<?php get_template_part('./template-parts/header'); ?>
 
-<main>
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-        <article>
-            <h2><?php the_title(); ?></h2>
-            <div><?php the_content(); ?></div>
-        </article>
-    <?php endwhile; endif; ?>
-</main>
-
-<?php get_footer(); ?>
+<?php get_template_part('./template-parts/footer'); ?>
