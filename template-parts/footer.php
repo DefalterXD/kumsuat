e69c7c5437624da2bd5e749c38770a46ff0b7198
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-04-08 04:42:16 +0500

    feat: add footer template

---
+++ template-parts/footer.php
@@ -0,0 +1,20 @@
+<footer class = "footer__container">
+    <nav class="nav__container">
+        <ul class="read__container">
+            <li class="read__link"><a href="">Terms of use</a></li>
+            <li class="read__link"><a href="">Privacy Policy</a></li>
+        </ul>
+        <div class="media__container">
+
+        </div>
+    </nav>
+    <div class="footer__desc__container">
+        <span class="footer__company">Kumsuat Co., Ltd. | Representative : Shon Kim</span>
+        <span class="footer__info">Address : 202 Hi-Tech Venture Town, 32 Soyanggang-ro, Chuncheon-si, Gangwon-do, 24232, Korea | Tel : 033-258-6114 | Business Identification Number : 221-81-38081</span>
+        <span class="footer__copyright">Copyright  2021 Chungdo Pharmaceutical Co., Ltd.. All rights reserved.</span>
+    </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
