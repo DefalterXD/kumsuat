0a80234f18e21fe72d30e0f5219faa24169994f9
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-05-11 19:28:58 +0500

    fix: localize the pages

--- template-parts/index__r&d.php
+++ template-parts/index__r&d.php
@@ -1,6 +1,8 @@
 <section class="rd__container">
     <div class="rd__card__container">
         <h1 class="rd__card__title">R&D</h1>
-        <h3 class="rd__card__desc">Explore our lab, where we conduct rigorous testing and quality control to ensure the highest standards in our in vitro diagnostic test papers.</h3>
+        <h3 class="rd__card__desc">Посетите нашу лабораторию, 
+            где мы проводим строгие испытания и контроль качества, 
+            чтобы гарантировать высочайшие стандарты наших диагностических тестов.</h3>
     </div>
 </section>
