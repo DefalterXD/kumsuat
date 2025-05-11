6dc9e23f39563e787b8c379f5c1d3b25d9d52818
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-05-11 19:17:17 +0500

    feat: add page for certificate view

---
+++ template-parts/certificate.php
@@ -0,0 +1,4 @@
+<section class="certificate__container">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/certificate.png" alt="certificate">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/certificate__2.png" alt="certificate">
+</section>
