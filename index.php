2332d7a3670ed9c763fb4f75fde62eae9cc5e180
Defalter <103436489+DefalterXD@users.noreply.github.com>
2025-05-11 19:28:30 +0500

    feat: add certificate to the main page
    Add certificate page to the main page and temporary put away r&d page

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 <?php get_template_part('./template-parts/carousel'); ?>
 <?php get_template_part('./template-parts/index__products'); ?>
-<?php get_template_part('./template-parts/index__r&d'); ?>
+<?php get_template_part('./template-parts/certificate'); ?>
+<!-- <?php get_template_part('./template-parts/index__r&d'); ?>  -->
 
 <?php get_template_part('./template-parts/footer'); ?>
